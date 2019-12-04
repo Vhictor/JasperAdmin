c8c1a697af88def0128aeb5f32f4e344fe17c130
victor <adeleyevictor40@gmail.com>
2019-12-04 09:41:39 +0000

    It process each data request after 20 secondds

--- app/src/main/java/ng/com/grazac/jasperadmin/DataService.java
+++ app/src/main/java/ng/com/grazac/jasperadmin/DataService.java
@@ -6,14 +6,40 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Build;
+import android.os.Handler;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class DataService extends Service {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class DataService extends Service{
 
     public static final String CHANNEL_ID = "DataService";
+    RequestQueue mQueue;
+    private Timer timer;
+    private TimerTask timerTask;
+    public int counter=0;
+    boolean done;
+
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -34,9 +60,39 @@ public class DataService extends Service {
                 .setContentIntent(pendingIntent)
                 .build();
 
+        startTimer();
         startForeground(1, notification);
 
-        return START_NOT_STICKY;
+        return START_STICKY;
+    }
+
+
+
+    public void startTimer() {
+        //set a new Timer
+        timer = new Timer();
+
+        //initialize the TimerTask's job
+        initializeTimerTask();
+
+        //schedule the timer, to wake up every 1 minute and the period is 10 seconds
+        timer.schedule(timerTask, 1000, 20000); //
+    }
+
+    public void initializeTimerTask() {
+        timerTask = new TimerTask() {
+            public void run() {
+                Log.i("in timer", "in timer ++++  "+ (counter++));
+                    getPendingData();
+                }
+        };
+    }
+
+    public void stoptimertask() {
+        if (timer != null) {
+            timer.cancel();
+            timer = null;
+        }
     }
 
     private void createNotificationChannel() {
@@ -55,11 +111,174 @@ public class DataService extends Service {
 
     @Override
     public void onCreate() {
-        super.onCreate();
+    mQueue = Volley.newRequestQueue(this);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        stoptimertask();
+        Toast.makeText(this, "Data service has been stopped", Toast.LENGTH_SHORT).show();
+
+
     }
+
+
+
+
+    private void getPendingData() {
+
+        String url = "https://jassperdata.com/embedded/getPending?secret_key=980fee286d26f20b03f88a0bbb26fb1aa30b945b4f2c02451010c8349fc11a1d";
+        final JsonObjectRequest data = new JsonObjectRequest(Request.Method.GET, url, null, new
+                Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            int error = response.getInt("err");
+                            String msg =response.getString("msg");
+                            if (error == 0 && !msg.isEmpty()){
+//                                Toast.makeText(getApplicationContext(), "Message from server : " +msg, Toast.LENGTH_SHORT).show();
+                                //getPendingData();
+
+                                return;
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        try {
+                            int id = response.getInt("id");
+                            String phone = response.getString("phone");
+                            String dataSize = response.getString("data");
+                            Log.d("Phone", "No" +phone);
+  //                          Toast.makeText(getApplicationContext(), "Phone no is " +phone, Toast.LENGTH_SHORT).show();
+                            sendTheMessage(id,phone,dataSize);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "Error " +error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        mQueue.add(data);
+    }
+
+
+
+    public void sendTheMessage(int id, String userNo, String dataSize){
+        String message = "";
+        String destinationAddress = "131";
+        if (dataSize.equalsIgnoreCase("1GB")){
+            message = "SMEC " +userNo+ " 1000 4172";
+        }else if (dataSize.equalsIgnoreCase("2GB")){
+            message = "SMED " +userNo+ " 2000 4172";
+        }else if (dataSize.equalsIgnoreCase("5GB")){
+            message = "SMEE " +userNo+ " 5000 4172";
+        } else if (dataSize.equalsIgnoreCase("3GB")){
+            forOneGB(userNo);
+            forTwoGB(userNo);
+            checkStatus(id,1);
+            return;
+        } else if (dataSize.equalsIgnoreCase("4GB")){
+            forTwoGB(userNo);
+            forTwoGB(userNo);
+            checkStatus(id,1);
+            return;
+        } else if (dataSize.equalsIgnoreCase("10GB")){
+            forFiveGB(userNo);
+            forFiveGB(userNo);
+            checkStatus(id,1);
+            return;
+
+        }else {
+            return;
+        }
+
+
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage(destinationAddress, null, message, null, null);
+//        Toast.makeText(getApplicationContext(), "SMS sent.",
+//                Toast.LENGTH_LONG).show();
+
+        if(message!=null){
+            checkStatus(id,1);
+        }
+
+
+    }
+
+
+    private void forOneGB(String userNo){
+
+        String message = "SMEC " +userNo+ " 1000 4172";
+        String destinationAddress = "131";
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage(destinationAddress, null, message, null, null);
+//        Toast.makeText(getApplicationContext(), "SMS sent.",
+//                Toast.LENGTH_LONG).show();
+
+    }
+
+    private void forTwoGB(String userNo){
+
+        String message = "SMED " +userNo+ " 2000 4172";
+        String destinationAddress = "131";
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage(destinationAddress, null, message, null, null);
+//        Toast.makeText(getApplicationContext(), "SMS sent.",
+//                Toast.LENGTH_LONG).show();
+
+    }
+
+    private void forFiveGB(String userNo){
+
+        String message = "SMEE " +userNo+ " 5000 4172";
+        String destinationAddress = "131";
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage(destinationAddress, null, message, null, null);
+        Toast.makeText(getApplicationContext(), "SMS sent.",
+                Toast.LENGTH_LONG).show();
+
+    }
+
+
+    private void checkStatus(int id, int status) {
+        String url = "https://jassperdata.com/embedded/updateStatus?secret_key=980fee286d26f20b03f88a0bbb26fb1aa30b945b4f2c02451010c8349fc11a1d&id=" +id + "&status=" +status;
+        final JsonObjectRequest dataStatus = new JsonObjectRequest(Request.Method.GET, url, null, new
+                Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            int err = response.getInt("err");
+                            if (err==0){
+                       //         Toast.makeText(getApplicationContext(), "Successful " +err, Toast.LENGTH_SHORT).show();
+
+                              //  getPendingData();
+                            }else {
+                                Toast.makeText(getApplicationContext(), "Unsuccessul " + err, Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "Error " +error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mQueue.add(dataStatus);
+    }
+
+
+
 }
--- app/src/main/java/ng/com/grazac/jasperadmin/MainActivity.java
+++ app/src/main/java/ng/com/grazac/jasperadmin/MainActivity.java
@@ -2,12 +2,14 @@ package ng.com.grazac.jasperadmin;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.IntentService;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -17,6 +19,8 @@ import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -31,71 +35,51 @@ import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button;
-    private RequestQueue mQueue;
+    Button button, buttonStop;
+    ProgressBar progressBar;
+    TextView mTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mQueue = Volley.newRequestQueue(this);
-
+        progressBar = findViewById(R.id.progress);
+        mTextView = findViewById(R.id.request_condition);
+        buttonStop = findViewById(R.id.button_stop);
         button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               getPendingData();
+                if (isNetworkConnected()){
+                    Toast.makeText(MainActivity.this, "Work has started, please don't press again", Toast.LENGTH_SHORT).show();
+                    progressBar.setVisibility(View.VISIBLE);
+                    sendSms();
+                }else {
+                    Toast.makeText(MainActivity.this, "Can't start service because there is no internet connection", Toast.LENGTH_SHORT).show();
+                }
+
                 //getPendingData();
             }
         });
 
-
-
-    }
-
-    private void getPendingData() {
-
-    String url = "https://jassperdata.com/embedded/getPending?secret_key=980fee286d26f20b03f88a0bbb26fb1aa30b945b4f2c02451010c8349fc11a1d";
-        final JsonObjectRequest data = new JsonObjectRequest(Request.Method.GET, url, null, new
-                Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            int error = response.getInt("err");
-                            String msg =response.getString("msg");
-                            if (error == 0 && !msg.isEmpty()){
-                             // Toast.makeText(MainActivity.this, "No pending data from website", Toast.LENGTH_SHORT).show();
-                                getPendingData();
-                                return;
-                            }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-
-                        try {
-                            int id = response.getInt("id");
-                            String phone = response.getString("phone");
-                            String dataSize = response.getString("data");
-                            Log.d("Phone", "No" +phone);
-                            Toast.makeText(MainActivity.this, "Phone no is " +phone, Toast.LENGTH_SHORT).show();
-                            sendTheMessage(id,phone,dataSize);
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-
-
-                    }
-                }, new Response.ErrorListener() {
+        buttonStop.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(MainActivity.this, "Error " +error.getMessage(), Toast.LENGTH_SHORT).show();
+            public void onClick(View view) {
+                stopService(new Intent(MainActivity.this,DataService.class));
+                progressBar.setVisibility(View.INVISIBLE);
             }
         });
-        mQueue.add(data);
+
     }
 
-    private void sendSms(int id, String phone, String dataSize) {
+
+    private boolean isNetworkConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnected();
+    }
+
+
+    private void sendSms() {
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.SEND_SMS)
                 != PackageManager.PERMISSION_GRANTED) {
@@ -107,7 +91,9 @@ public class MainActivity extends AppCompatActivity {
                         1);
             }
         }else {
-            sendTheMessage(id, phone, dataSize);
+//            sendTheMessage(1, "09069167788", "1GB");
+            Intent intent = new Intent(this,DataService.class);
+            startService(intent);
         }
 
     }
@@ -119,7 +105,8 @@ public class MainActivity extends AppCompatActivity {
             case 1: {
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                        sendTheMessage(1,  "09069167788", "1GB");
+                    Intent intent = new Intent(this,DataService.class);
+                    startService(intent);
                 } else {
                     Toast.makeText(getApplicationContext(),
                             "SMS failed, please try again.", Toast.LENGTH_LONG).show();
@@ -131,187 +118,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void sendTheMessage(int id, String userNo, String dataSize){
-        String message = "";
-        String destinationAddress = "131";
-        if (dataSize.equalsIgnoreCase("1GB")){
-            message = "SMEC " +userNo+ " 1000 4172";
-        }else if (dataSize.equalsIgnoreCase("2GB")){
-            message = "SMED " +userNo+ " 2000 4172";
-        }else if (dataSize.equalsIgnoreCase("5GB")){
-            message = "SMEE " +userNo+ " 5000 4172";
-        } else if (dataSize.equalsIgnoreCase("3GB")){
-            forOneGB(userNo);
-            forTwoGB(userNo);
-            checkStatus(id,1);
-            return;
-        } else if (dataSize.equalsIgnoreCase("4GB")){
-            forTwoGB(userNo);
-            forTwoGB(userNo);
-            checkStatus(id,1);
-            return;
-        } else if (dataSize.equalsIgnoreCase("10GB")){
-            forFiveGB(userNo);
-            forFiveGB(userNo);
-            checkStatus(id,1);
-            return;
-
-        }else {
-            return;
-        }
-
-
-        SmsManager smsManager = SmsManager.getDefault();
-        smsManager.sendTextMessage(destinationAddress, null, message, null, null);
-        Toast.makeText(getApplicationContext(), "SMS sent.",
-                Toast.LENGTH_LONG).show();
-
-        if(message!=null){
-            checkStatus(id,1);
-        }
-
-
-    }
-
-
-    private void forOneGB(String userNo){
-
-        String message = "SMEC " +userNo+ " 1000 4172";
-        String destinationAddress = "131";
-        SmsManager smsManager = SmsManager.getDefault();
-        smsManager.sendTextMessage(destinationAddress, null, message, null, null);
-        Toast.makeText(getApplicationContext(), "SMS sent.",
-                Toast.LENGTH_LONG).show();
-
-    }
-
-    private void forTwoGB(String userNo){
-
-        String message = "SMED " +userNo+ " 2000 4172";
-        String destinationAddress = "131";
-        SmsManager smsManager = SmsManager.getDefault();
-        smsManager.sendTextMessage(destinationAddress, null, message, null, null);
-        Toast.makeText(getApplicationContext(), "SMS sent.",
-                Toast.LENGTH_LONG).show();
-
-    }
-
-    private void forFiveGB(String userNo){
-
-        String message = "SMEE " +userNo+ " 5000 4172";
-        String destinationAddress = "131";
-        SmsManager smsManager = SmsManager.getDefault();
-        smsManager.sendTextMessage(destinationAddress, null, message, null, null);
-        Toast.makeText(getApplicationContext(), "SMS sent.",
-                Toast.LENGTH_LONG).show();
-
-    }
-
-
-    private void checkStatus(int id, int status) {
-        String url = "https://jassperdata.com/embedded/updateStatus?secret_key=980fee286d26f20b03f88a0bbb26fb1aa30b945b4f2c02451010c8349fc11a1d&id=" +id + "&status=" +status;
-        final JsonObjectRequest dataStatus = new JsonObjectRequest(Request.Method.GET, url, null, new
-                Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            int err = response.getInt("err");
-                            if (err==0){
-                                Toast.makeText(MainActivity.this, "Successful " +err, Toast.LENGTH_SHORT).show();
-                                getPendingData();
-                            }else {
-
-                                Toast.makeText(MainActivity.this, "Unsuccessul " + err, Toast.LENGTH_SHORT).show();
-                            }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-
-
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(MainActivity.this, "Error " +error.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        mQueue.add(dataStatus);
-    }
-
-//    private void sendSMS(final int id, String userNo, String dataSize) {
-//        String message = "";
-//        String destinationAddress = "131";
-//        if (dataSize.equalsIgnoreCase("1GB")){
-//            message = "SMEC " +userNo+ " 1000 4172";
-//        }else if (dataSize.equalsIgnoreCase("2GB")){
-//            message = "SMED " +userNo+ " 2000 4172";
-//        }else if (dataSize.equalsIgnoreCase("5GB")){
-//            message = "SMEE " +userNo+ " 5000 4172";
-//        }
-//
-//
-//        String SENT = "SMS_SENT";
-//        String DELIVERED = "SMS_DELIVERED";
-//
-//        final PendingIntent sentPI = PendingIntent.getBroadcast(this, 0,
-//                new Intent(SENT), 0);
-//
-//        PendingIntent deliveredPI = PendingIntent.getBroadcast(this, 0,
-//                new Intent(DELIVERED), 0);
-//
-//        //---when the SMS has been sent---
-//        registerReceiver(new BroadcastReceiver(){
-//            @Override
-//            public void onReceive(Context arg0, Intent arg1) {
-//                switch (getResultCode())
-//                {
-//                    case Activity.RESULT_OK:
-//                        Toast.makeText(getBaseContext(), "SMS sent",
-//                                Toast.LENGTH_SHORT).show();
-//                        break;
-//                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
-//                        Toast.makeText(getBaseContext(), "Generic failure",
-//                                Toast.LENGTH_SHORT).show();
-//                        break;
-//                    case SmsManager.RESULT_ERROR_NO_SERVICE:
-//                        Toast.makeText(getBaseContext(), "No service",
-//                                Toast.LENGTH_SHORT).show();
-//                        break;
-//                    case SmsManager.RESULT_ERROR_NULL_PDU:
-//                        Toast.makeText(getBaseContext(), "Null PDU",
-//                                Toast.LENGTH_SHORT).show();
-//                        break;
-//                    case SmsManager.RESULT_ERROR_RADIO_OFF:
-//                        Toast.makeText(getBaseContext(), "Radio off",
-//                                Toast.LENGTH_SHORT).show();
-//                        break;
-//                }
-//            }
-//        }, new IntentFilter(SENT));
-//
-//        //---when the SMS has been delivered---
-//        registerReceiver(new BroadcastReceiver(){
-//            @Override
-//            public void onReceive(Context arg0, Intent arg1) {
-//                switch (getResultCode())
-//                {
-//                    case Activity.RESULT_OK:
-//                        Toast.makeText(getBaseContext(), "SMS delivered",
-//                                Toast.LENGTH_SHORT).show();
-//                        break;
-//                    case Activity.RESULT_CANCELED:
-//                        Toast.makeText(getBaseContext(), "SMS not delivered",
-//                                Toast.LENGTH_SHORT).show();
-//                        break;
-//                }
-//            }
-//        }, new IntentFilter(DELIVERED));
-//
-//        SmsManager sms = SmsManager.getDefault();
-//        sms.sendTextMessage(destinationAddress, null, message, sentPI, deliveredPI);
-//
-//    }
 
 
 }
